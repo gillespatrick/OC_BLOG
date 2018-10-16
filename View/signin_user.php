f9ba87fac7a0975d8f7454cf0a249ef8399c6b99
root <root@Debian.titan>
2018-10-16 07:10:01 -0400

    rename file signup to signin.php

---
+++ View/signin_user.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>SIGN IN USER</title>
+     <!-- Bootstrap core CSS -->
+     <link href="http://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+<!-- Custom styles for this template -->
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+<link href="http://getbootstrap.com/docs/4.1/examples/blog/blog.css" rel="stylesheet"> 
+<link href="http://getbootstrap.com/docs/4.1/examples/sign-in/signin.css" rel="stylesheet">
+  </head>
+  <body>
+  
+  <div class="container">
+      <header class="blog-header py-3">
+        <div class="row flex-nowrap justify-content-between align-items-center">
+          <div class="col-4 pt-1">
+            
+          </div>
+          <div class="col-4 text-center">
+            <a class="blog-header-logo text-dark" href="../index.php">Gilles Patrick</a>
+          </div>
+          <div class="col-4 d-flex justify-content-end align-items-center">
+            <a class="text-muted" href="#">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
+            </a>
+            <a class="btn btn-sm btn-outline-secondary" href="#">Sign in</a>
+          </div>
+        </div>
+      </header> 
+  
+  <form class="form-signin">
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus><br/>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      
+      <button class="btn btn-lg btn-primary btn-success" type="submit">Sign in</button>
+
+      <a href="forgot.php" class="text-success">Forgot your password?</a>
+ 
+     <p class="mt-5 mb-3 text-muted">&copy;Gilles Patrick  2018</p>
+
+    </form>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
