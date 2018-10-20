ad6fdb803e8c633b6846a7882908d51949579dd7
Patrick <patrick@Debian.titan>
2018-10-20 09:55:56 -0400

    Modify a link

--- View/header.php
+++ View/header.php
@@ -11,7 +11,7 @@
             <a class="text-muted" href="#">
               
             </a>
-            <a class="btn btn-sm btn-outline-secondary" href="View/signup.php">Sign up</a>
+            <a class="btn btn-sm btn-outline-secondary" href="View/open_account.php">Sign up</a>
           </div>
         </div>
       </header>
