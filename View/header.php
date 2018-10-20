81eab0cd9f58dcdf8cffee0f36050d09ee8db8e9
Patrick <patrick@Debian.titan>
2018-10-19 23:06:27 -0400

    nothing

--- View/header.php
+++ View/header.php
@@ -19,7 +19,7 @@
       <div class="nav-scroller py-1 mb-2">
         <nav class="nav d-flex justify-content-between">
           
-          <a class="p-2 text-muted" href="/index.php">Home</a>
+          <a class="p-2 text-muted" href="../index.php">Home</a>
           <a class="p-2 text-muted" href="#">Categories</a>
           <a class="p-2 text-muted" href="#">Curriculum vitae</a>
           <a class="p-2 text-muted" href="#">Contact</a>
