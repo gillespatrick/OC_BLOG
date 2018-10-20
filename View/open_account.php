bb4e7448f164aeafb99ab1453ddebe4a09f5af65
Patrick <patrick@Debian.titan>
2018-10-20 12:54:09 -0400

    modify a open account form

--- View/open_account.php
+++ View/open_account.php
@@ -2,7 +2,7 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
-    <title>Administrator</title>
+    <title>OPEN ACCOUNT</title>
      <!-- Bootstrap core CSS -->
      <link href="http://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -21,32 +21,34 @@
             
           </div>
           <div class="col-4 text-center">
-            <a class="blog-header-logo text-dark" href="">Gilles Patrick</a>
+            <a class="blog-header-logo text-dark" href="../index.php">Gilles Patrick</a>
           </div>
           <div class="col-4 d-flex justify-content-end align-items-center">
             <a class="text-muted" href="#">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
             </a>
-            
+            <a class="btn btn-sm btn-outline-secondary" href="signin.php">Have you account?</a>
           </div>
         </div>
-      </header> 
+      </header> <br/><br/><br/>
   
   <form class="form-signin">
-      <h1 class="h3 mb-3 font-weight-normal"> Login and Password</h1>
+      <h1 class="h3 mb-3 font-weight-normal"> Enrollement</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus><br/>
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-      <input type="password" id="inputPasswordConf" class="form-control" placeholder="Confirmation Password" required>
+      <label for="inputPasswordConf" class="sr-only">confirmation Password</label>
+      <input type="password" id="inputPasswordconf" class="form-control" placeholder="Confirmation Password" required>
+     
       <div class="checkbox mb-3">
-        <label>
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
+        
       </div>
-      <button class="btn btn-lg btn-primary btn-success" type="submit">Sign in</button>
+      <button class="btn btn-lg btn-primary btn-success" type="submit">Enroll</button>
       <p class="mt-5 mb-3 text-muted">&copy; Gilles Patrick 2018</p>
     </form>
+
+    
         </div>
       </div>
     </div>
