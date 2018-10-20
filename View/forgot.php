380d064056799e49d501cdcb00f758cf4575e11b
Patrick <patrick@Debian.titan>
2018-10-20 13:03:16 -0400

    add link to sign in a open account

--- View/forgot.php
+++ View/forgot.php
@@ -2,7 +2,7 @@
 <html lang="fr">
   <head>
     <meta charset="utf-8">
-    <title>Forgot your password</title>
+    <title>FORGOT PASSWORD</title>
      <!-- Bootstrap core CSS -->
      <link href="http://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -24,19 +24,18 @@
             <a class="blog-header-logo text-dark" href="../index.php">Gilles Patrick</a>
           </div>
           <div class="col-4 d-flex justify-content-end align-items-center">
-            <a class="text-muted" href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
-            </a>
-            <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+           
+            <a class="btn btn-sm btn-outline-secondary" href="signin.php">Sign in</a>
+            <a class="btn btn-sm btn-outline-secondary" href="open_account.php">Open Acount</a>
           </div>
         </div>
-      </header> 
+      </header> <br/><br/>
   
   <form class="form-signin">
       <h1 class="h3 mb-3 font-weight-normal">Please enter your address!</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus><br/>
-      <button class="btn btn-lg btn-primary btn-success" type="submit">Submit</button>
+      <button class="btn btn-lg btn-primary btn-success" type="submit">Send</button>
       <p class="mt-5 mb-3 text-muted">&copy;Gilles Patrick  2018</p>
     </form>
         </div>
