a99c784d5ff035d4f5bdc525243ce1b4747c6320
Patrick <patrick@Debian.titan>
2018-10-21 06:41:37 -0400

    add the button

---
+++ View/profil_user.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title>MY USER PROFIL</title>
+     <!-- Bootstrap core CSS -->
+     <link href="http://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+<!-- Custom styles for this template -->
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+<link href="http://getbootstrap.com/docs/4.1/examples/blog/blog.css" rel="stylesheet"> 
+<link href="http://getbootstrap.com/docs/4.1/examples/sign-in/signin.css" rel="stylesheet">
+  </head>
+  <body>
+  
+  <div class="container">
+      <header class="blog-header py-3">
+        <div class="row flex-nowrap justify-content-between align-items-center">
+          <div class="col-4 pt-1"> 
+          </div>
+          <div class="col-4 text-center">
+            <a class="blog-header-logo text-dark" href="#">GILLES PATRICK</a>
+          </div>
+          <div class="col-4 d-flex justify-content-end align-items-center">
+           
+            
+          </div>
+        </div>
+      </header> <br/><br/>
+  
+      <div class="container profil">
+      <h1>My Account</h1>
+     <!-- <a href="avatar.php" class="btn btn-success">Changer mon image de profil</a> -->
+      <a href="MyInformations.php" class="btn btn-success">My informations</a>
+    </div>
+
+    
+        </div>
+      </div>
+    </div>
+    
+  </body>
+</html>
