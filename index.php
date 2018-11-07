3e5f63eb38489e794fd5b520fffd79c70dc707a0
Patrick <patrick@Debian.titan>
2018-11-07 12:46:03 -0500

    Create index file

--- index.php
+++ index.php
@@ -1,79 +1,19 @@
-
-<!doctype html>
-<html lang="en">
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-   
-
-    <title>HOME</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="http://getbootstrap.com/docs/4.1/dist/css/bootstrap.min.css" rel="stylesheet">
-
-
-    <!-- Custom styles for this template -->
-    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
-    <link href="http://getbootstrap.com/docs/4.1/examples/blog/blog.css" rel="stylesheet">
+    <title>GILLES PATRICK</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,700,300">
+    <link rel="stylesheet" href="css/style.css">
   </head>
-
   <body>
-
-   <?php include 'View/header.php';?>
-
-      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark">
-        <div class="col-md-6 px-0">
-          <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-          <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
-          <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
-        </div>
-      </div>
-
-    <footer class="blog-footer">
-    <footer class="site-footer bg-darkest" role="contentinfo">
+    <?php include 'functions/header.php' ?>
     <div class="container">
-
-      <ul class="nav justify-content-center">
-        <li class="nav-item">
-          <a class="nav-link" href="https://github.com/gillespatrick">Mes repositories</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.linkedin.com/in/GPNgondo">Linkedin</a>
-        </li>
-
-         <li class="nav-item">
-          <a class="nav-link" href="View/admin.php">Admin Page</a>
-        </li>
-        
-        <li class="nav-item">
-          <a class="nav-link" href="page-contact.html">Contact</a>
-        </li>
-      </ul>
-      <div class="copy">
-        &copy; Gilles Patrick 2018<br />
-        All rights reserved
-      </div>
+      <h1>Ma page d'accueil</h1>
     </div>
-  </footer>
-      
-    </footer>
-
-   <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="http://getbootstrap.com/docs/4.1/assets/js/vendor/popper.min.js"></script>
-    <script src="http://getbootstrap.com/docs/4.1/dist/js/bootstrap.min.js"></script>
-    <script src="http://getbootstrap.com/docs/4.1/assets/js/vendor/holder.min.js"></script>
-    <script>
-      Holder.addTheme('thumb', {
-        bg: '#55595c',
-        fg: '#eceeef',
-        text: 'Thumbnail'
-      });
-    </script>
   </body>
 </html>
