0726431677aa27f9600406f01929c34e055b7912
Patrick <patrick@Debian.titan>
2018-11-07 16:14:00 -0500

    Put a link to sign up

--- functions/header.php
+++ functions/header.php
@@ -11,7 +11,7 @@
       </li>
       <?php else : ?>
       <li class="nav-item">
-        <a class="nav-link" href="inscription.php">Inscription</a>
+        <a class="nav-link" href="signup.php">SIGN UP</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="connexion.php">Connexion</a>
