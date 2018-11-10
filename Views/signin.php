2d2cc76bb67a46ce6f4775c95315ab9ffaf1757d
Patrick <patrick@Debian.titan>
2018-11-10 15:49:31 -0500

    ok

--- Views/signin.php
+++ Views/signin.php
@@ -28,10 +28,10 @@ if ( !empty( $_POST ) ) {
     <title>SIGN IN</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,700,300">
-    <link rel="stylesheet" href="Views/css/style.css">
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-    <?php include 'Views/inc/header.php' ?>
+    <?php include 'inc/header.php'; ?>
     <div class="container">
       <h1 class="text-xs-center">SIGN IN</h1>
       <div class="row">
