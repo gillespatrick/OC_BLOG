53b6af10da01c4c408f9e2dc1415e9316d141657
Patrick <patrick@Debian.titan>
2018-12-01 10:44:58 -0500

    Create class user

---
+++ models/User.php
@@ -0,0 +1,84 @@
+<?php
+
+
+
+
+class User 
+{
+    private $id;
+    private $first_name;
+    private $last_ame;
+    private $email;
+    private $password;
+
+    // GETTERS
+
+    public function getId()
+    {
+        return $this -> id;
+    }
+
+    public function getFirst_name()
+    {
+        return $this -> first_name;
+    }
+
+    public function getLast_name()
+    {
+        return $this -> last_name;
+    }
+
+
+    public function getEmail()
+    {
+        return $this -> email;
+    }
+
+
+    public function getPassword()
+    {
+        return $this-> password;
+    }
+
+
+
+    // SETTERS
+
+    public function setId($id)
+    {
+        $this-> id = (int) $id;
+    }
+
+    public function setFirst_name($first_name)
+    {
+        if (is_string($first_name)) {
+            $this -> first_name = $first_name;
+        }
+    }
+
+    public function setLast_name($last_name)
+    {
+        if (is_string($firstName)) {
+            $this -> last_name = $last_name;
+        }
+    }
+
+  
+
+    public function setEmail($email)
+    {
+        if (is_string($mail)) {
+            $this->email = $email;
+        }
+    }
+
+   
+
+   /* public function setPassword($password)
+    {
+        $this->password = password_hash($password, PASSWORD_DEFAULT);
+    }*/
+
+
+    
+}
