4f9b27b459f71c39607cff7a9fa9680746ebedba
Gilles Patrick <ngondogillespatrick@gmail.com>
2019-02-28 19:44:16 -0500

    Create my home page

---
+++ app/views/blog.php
@@ -0,0 +1,81 @@
+<?php include 'app/views/inc/header.php'; ?>
+
+
+
+
+<h1>MY BLOG</h1>
+<hr />
+
+<article class="postcontent">
+    <div>
+        <h2><a href="#">Title of the post content </a></h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate ante maximus molestie fermentum. Nunc fermentum ligula quis ultricies ultricies. Phasellus aliquet
+            ante id nisi volutpat, eget ultricies urna euismod. In et ligula ligula. Aliquam ac ipsum pretium, iaculis enim sed, suscipit turpis. Aliquam ante nisi, tristique eget dui vitae, facilisis vestibulum arcu. Aenean vel odio
+            vestibulum, accumsan lectus non, faucibus orci. Mauris id ante non massa congue
+            luctus ut vitae purus. Duis eget tellus pellentesque, vehicula elit ut, auctor mauris. Curabitur
+            consectetur turpis vel odio ullamcorper, quis convallis nibh aliquet. Quisque dignissim magna pharetra pharetra fringilla. Praesent efficitur
+            magna quis erat hendrerit, a viverra ex pellentesque. Aliquam ac justo blandit, porta sapien ut, dignissim metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+            Vivamus euismod id enim ut accumsan. Nunc id varius tortor. Aliquam erat volutpat. Fusce tristique vitae arcu ac mollis. Morbi quam nisl, vulputate quis auctor non, semper eget erat. Donec rutrum quam at neque finibus, vitae ornare sapien viverra. Praesent tristique,
+            lectus ac porta iaculis, ipsum quam malesuada dui, eget rutrum metus massa a nunc.
+            Duis sodales condimentum quam vitae euismod. In hac habitasse platea dictumst. Mauris suscipit auctor congue. Aliquam molestie sem sed purus eleifend fermentum. Etiam a tellus ex. Ut mollis sed metus egestas efficitur. Ut pretium fringilla nisl.
+
+        </p>
+        <div class="readmore"><a href="#" </a> Read More</div> </div> <div>
+                <h2><a href="#">Title of the post content </a></h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vulputate ante maximus molestie fermentum. Nunc fermentum ligula quis ultricies ultricies. Phasellus aliquet
+                    ante id nisi volutpat, eget ultricies urna euismod. In et ligula ligula. Aliquam ac ipsum pretium, iaculis enim sed, suscipit turpis. Aliquam ante nisi, tristique eget dui vitae, facilisis vestibulum arcu. Aenean vel odio
+                    vestibulum, accumsan lectus non, faucibus orci. Mauris id ante non massa congue
+                    luctus ut vitae purus. Duis eget tellus pellentesque, vehicula elit ut, auctor mauris. Curabitur
+                    consectetur turpis vel odio ullamcorper, quis convallis nibh aliquet. Quisque dignissim magna pharetra pharetra fringilla. Praesent efficitur
+                    magna quis erat hendrerit, a viverra ex pellentesque. Aliquam ac justo blandit, porta sapien ut, dignissim metus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                    Vivamus euismod id enim ut accumsan. Nunc id varius tortor. Aliquam erat volutpat. Fusce tristique vitae arcu ac mollis. Morbi quam nisl, vulputate quis auctor non, semper eget erat. Donec rutrum quam at neque finibus, vitae ornare sapien viverra. Praesent tristique,
+                    lectus ac porta iaculis, ipsum quam malesuada dui, eget rutrum metus massa a nunc.
+                    Duis sodales condimentum quam vitae euismod. In hac habitasse platea dictumst. Mauris suscipit auctor congue. Aliquam molestie sem sed purus eleifend fermentum. Etiam a tellus ex. Ut mollis sed metus egestas efficitur. Ut pretium fringilla nisl.
+
+                </p>
+                <div class="readmore"><a href="#" </a> Read More</div> </div> 
+            
+    </article>
+         
+    
+    <aside class="sidebar">
+        <div class ="widget">
+            <h2>Category</h2>
+            <ul>
+                <li> <a href="#"> Category one </a> </li>
+                <li> <a href="#"> Category one </a> </li>
+                <li> <a href="#"> Category one </a> </li>
+                <li> <a href="#"> Category one </a> </li>
+                <li> <a href="#"> Category one </a> </li>
+                <li> <a href="#"> Category one </a> </li>   
+
+
+            </ul>
+           
+        </div>
+
+
+
+
+         <div class ="widget">
+            <h2>Latest Post</h2>
+            <ul>
+                <li> <a href="#"> Title of the post </a> </li>
+                <li> <a href="#"> Title of the post </a> </li>
+                <li> <a href="#"> Title of the post </a> </li>
+                <li> <a href="#"> Title of the post </a> </li>
+                <li> <a href="#"> Title of the post </a> </li>
+                <li> <a href="#"> Title of the post </a> </li>  
+
+
+            </ul>
+           
+        </div>
+
+
+     </aside>
+
+
+
+
+<?php include 'app/views/inc/footer.php'; ?> 
