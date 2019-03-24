01735a490e6975e403cc3680a49ba3698fdb52d0
Gilles Patrick <ngondogillespatrick@gmail.com>
2019-03-24 13:16:26 -0400

    update

--- app/views/post.php
+++ app/views/post.php
@@ -21,43 +21,7 @@
     </article>
          
     
-    <aside class="sidebar">
-        <div class ="widget">
-            <h2>Category</h2>
-            <ul>
-                <li> <a href="#"> Category one </a> </li>
-                <li> <a href="#"> Category one </a> </li>
-                <li> <a href="#"> Category one </a> </li>
-                <li> <a href="#"> Category one </a> </li>
-                <li> <a href="#"> Category one </a> </li>
-                <li> <a href="#"> Category one </a> </li>   
-
-
-            </ul>
-           
-        </div>
-
-
-
-
-         <div class ="widget">
-            <h2>Latest Post</h2>
-            <ul>
-                <li> <a href="#"> Title of the post </a> </li>
-                <li> <a href="#"> Title of the post </a> </li>
-                <li> <a href="#"> Title of the post </a> </li>
-                <li> <a href="#"> Title of the post </a> </li>
-                <li> <a href="#"> Title of the post </a> </li>
-                <li> <a href="#"> Title of the post </a> </li>  
-
-
-            </ul>
-           
-        </div>
-
-
-     </aside>
-
+    
 
 
 
